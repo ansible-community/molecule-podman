426d1938545d1ecd2aca1231692b6063afc92f5a
Sorin Sbarnea <ssbarnea@redhat.com>
2020-10-20 10:43:43 +0100

    Moved package under lib folder
    Follow source layout used by upstream projects.

--- lib/molecule_podman/test/conftest.py
+++ lib/molecule_podman/test/conftest.py
@@ -1,17 +1,6 @@
 """Pytest Fixtures."""
-import os
-import platform
-
 import pytest
 from molecule.test.conftest import random_string, temp_dir  # noqa
-
-
-def pytest_collection_finish(session):
-    """Fail fast if current environment is broken."""
-    if "CONTAINER_HOST" in os.environ and platform.system() != "Darwin":
-        pytest.exit(
-            msg="CONTAINER_HOST is defined, see https://github.com/containers/podman/issues/8070"
-        )
 
 
 @pytest.fixture
